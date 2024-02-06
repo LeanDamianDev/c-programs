9d1b7b8967c4364fce449fb5f6cd749966cca07a
LeanDamianDev <125818512+LeanDamianDev@users.noreply.github.com>
2024-02-06 02:45:01 -0300

    Add files via upload

---
+++ C++/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	string salida;
+	cout << "IMPORTANTE LEER";
+	cout << "\n";
+	cout << "-----------------------";
+	cout << "\n";
+	cout << "-----------------------" << "\n";
+	cout << "- USAR CON FINES EDUCATIVOS" << "\n";
+	cout << "- RECUERDEN EVITAR USAR LA PC MIENTRAS ESTA CARGANDO";
+	cout << "\n";
+	cout << "- NO DEJAR ARCHIVOS PERSONALES";
+	cout << "\n";
+	cout << "-----------------------";
+	cout << "\n";
+	cout << "- UNA VEZ QUE SE DEJA DE USAR LA PC, LLEVAR A DEVOLVERLA Y APAGARLA";
+	cout << "\n";	
+	cout << "-----------------------";
+	cout << "\n";
+	cout << "GRACIAS";
+	cout << "\n";
+	cout << "\n";
+	cin >> salida;	
+	return 0;
+}
